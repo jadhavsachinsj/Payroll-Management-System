d17c5aa753eb44798df873feac41b84e90aea59d
jadhavsachinsj <35762086+jadhavsachinsj@users.noreply.github.com>
2018-07-26 21:57:28 +0530

    Update settings.py

--- payroll/settings.py
+++ payroll/settings.py
@@ -155,8 +155,8 @@ MESSAGE_TAGS = {
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jadhavsachinsj47@gmail.com'
-EMAIL_HOST_PASSWORD = '7028116829'
+EMAIL_HOST_USER = 'MyEmail'
+EMAIL_HOST_PASSWORD = 'Password'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
